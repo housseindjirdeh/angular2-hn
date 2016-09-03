d08ff116bba0bbf9d2600a6334a7208e43298be1
hdjirdeh <h.djirdeh@mail.utoronto.ca>
2016-09-03 14:21:53 -0400

    style ext sass, set up header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
