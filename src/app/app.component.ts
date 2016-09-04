16fa16c2aa9153a08b74bbecdffff2d8a48b3a64
hdjirdeh <h.djirdeh@mail.utoronto.ca>
2016-09-04 00:07:02 -0400

    complete header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
 }
