9847cb84dcf5f3bac010cba476c7e0c0c294a0a6
Christos Koutsiaris <koutsiaris@gmail.com>
2016-09-22 02:54:12 +0100

    Url may not exist (#12)
    Fix when url not present - first page setup

--- src/app/domain.pipe.ts
+++ src/app/domain.pipe.ts
@@ -6,7 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DomainPipe implements PipeTransform {
 
   transform(url: any, args?: any): any {
-  	var domain = '(' + url.split('/')[2] + ')';
-    return domain ? domain.replace('www.', '') : '';
+    if (url) {
+      var domain = '(' + url.split('/')[2] + ')';
+      return domain ? domain.replace('www.', '') : '';
+    }
   }
 }
