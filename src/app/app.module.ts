486ba015e25d38663edc396ee53d5f9443213e76
Jordi Collell <jordic@gmail.com>
2016-12-12 06:31:42 +0100

    form out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { routing } from './app.routes';
