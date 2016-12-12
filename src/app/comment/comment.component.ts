181bfc1d0bf3b7c8b6c948f7de67085137a821a7
jordi <jordic@gmail.com>
2016-12-12 17:49:25 +0100

    icons for service worker

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.scss']
+  styleUrls: ['./comment.component.scss'],
 })
 export class CommentComponent implements OnInit {
   @Input() comment;
