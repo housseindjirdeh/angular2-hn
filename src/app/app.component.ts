45e236c0faebb09f11565761e70ed7bc00d9b7d2
Houssein Djirdeh <houssein@Housseins-MacBook-Pro.local>
2016-12-11 20:20:33 -0500

    remove view encapsulation none

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  encapsulation: ViewEncapsulation.None,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
