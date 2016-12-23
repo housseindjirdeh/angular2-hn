4b919047237112ea6c36bbb31faf9dba56af96a2
Houssein Djirdeh <h.djirdeh@mail.utoronto.ca>
2016-12-22 20:39:53 -0500

    set up service worker for static assets

---
+++ sw-precache-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  navigateFallback: '/index.html',
+  stripPrefix: 'dist',
+  root: 'dist/',
+  staticFileGlobs: [
+    'dist/index.html',
+    'dist/**.js',
+    'dist/**.css',
+    'dist/assets/images/**.png'
+  ]
+};
