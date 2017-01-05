88e68d79b9a7f0b09129648cf50a3fe3791ff9bf
Houssein Djirdeh <h.djirdeh@mail.utoronto.ca>
2017-01-05 13:21:57 -0500

    fix sw config bug

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,17 +1,16 @@
 module.exports = {
   staticFileGlobs: [
-    'dist/**.html',
+    'dist/index.html',
     'dist/**.js',
     'dist/**.css',
     'dist/assets/images/*',
     'dist/assets/icons/*'
   ],
-  root: 'dist'
+  root: 'dist',
   stripPrefix: 'dist/',
   navigateFallback: '/index.html',
   runtimeCaching: [{
     urlPattern: /node-hnapi\.herokuapp\.com/,
     handler: 'networkFirst'
-  }],
-  verbose: true
+  }]
 };
