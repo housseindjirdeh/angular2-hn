38df72ef2fafe1f3ac28601657f65b2f9732f822
Ashwin Sureshkumar <ashwin-sureshkumar@users.noreply.github.com>
2017-02-16 16:28:25 -0500

    Adding missed reference of router module in core (#51)

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { SettingsComponent } from './settings/settings.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [HeaderComponent, FooterComponent, SettingsComponent],
   exports: [HeaderComponent, FooterComponent]
 })
