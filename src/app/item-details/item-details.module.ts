dfe82eadd1cbcb75df09e2c641ba9869121d9bf2
Ashwin Sureshkumar <ashwin-sureshkumar@users.noreply.github.com>
2017-02-16 14:22:44 -0500

    Item details refactor and comment tree removal (#47)
    * Implement feeds module and refactor routes to reflect
    
    * Update story-type to feedtype and update references
    
    * Refactor item-comments to item-details, remove comment-tree component

--- src/app/item-details/item-details.module.ts
+++ src/app/item-details/item-details.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { ItemCommentsComponent } from './item-comments.component';
-import { CommentTreeComponent } from './comment-tree/comment-tree.component';
+import { ItemDetailsComponent } from './item-details.component';
 import { CommentComponent } from './comment/comment.component';
 import { SharedComponentsModule } from '../shared/components/shared-components.module';
 import { PipesModule } from '../shared/pipes/pipes.module';
+
 @NgModule({
   imports: [ SharedComponentsModule, CommonModule, RouterModule, PipesModule ],
-  declarations: [ ItemCommentsComponent, CommentTreeComponent, CommentComponent ],
-  exports: [ ItemCommentsComponent]
+  declarations: [ ItemDetailsComponent, CommentComponent ],
+  exports: [ ItemDetailsComponent]
 })
-export class ItemCommentsModule {}
+export class ItemDetailsModule {}
