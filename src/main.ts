2dc22f9955efb125beb73b6d9a559e8aad2efdbc
Majid <mhadaily@gmail.com>
2017-05-20 16:09:58 +0200

    register sw after bootstrapModule to boost initial load

--- src/main.ts
+++ src/main.ts
@@ -9,4 +9,12 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then(registration => console.log('SW registration successful with scope: ', registration.scope));
+    }
+  })
+  .catch(err => console.error(err));
