a0b12e583eea30914c0db1072cf2e79ad7341800
Jeff Cross <middlefloor@gmail.com>
2017-06-01 20:16:55 -0400

    perf(feed): move feed routes into app.module
    Since the feed routes are required for the first screen of the 
    application, lazy loading the feed components will most likely add more 
    latency to the time to bootstrap the home page. This commit moves the
    feed components into the AppModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,24 @@ import { routing } from './app.routes';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { ServicesModule } from './shared/services/services.module';
+import { FeedComponent } from "app/feeds/feed/feed.component";
+import { ItemComponent } from "app/feeds/item/item.component";
+import { SharedComponentsModule } from "app/shared/components/shared-components.module";
+import { PipesModule } from "app/shared/pipes/pipes.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
     routing,
     ServicesModule.forRoot(),
-    CoreModule
+    CoreModule,
+    SharedComponentsModule,
+    PipesModule
   ],
   providers: [ ],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,16 +4,42 @@ import { Routes, RouterModule } from '@angular/router';
 import { FeedComponent } from './feeds/feed/feed.component';
 import { UserComponent } from './user/user.component';
 
+const feedRoutes = [{
+  path: ':page',
+  component: FeedComponent
+}];
+
 const routes: Routes = [
   {path: '', redirectTo: 'news/1', pathMatch: 'full'},
-  {path: 'news', loadChildren: 'app/feeds/feeds.module#FeedsModule', data: {feedType: 'news'}},
-  {path: 'newest', loadChildren: 'app/feeds/feeds.module#FeedsModule', data: {feedType: 'newest'}},
-  {path: 'show', loadChildren: 'app/feeds/feeds.module#FeedsModule', data: {feedType: 'show'}},
-  {path: 'ask', loadChildren: 'app/feeds/feeds.module#FeedsModule', data: {feedType: 'ask'}},
-  {path: 'jobs', loadChildren: 'app/feeds/feeds.module#FeedsModule', data: {feedType: 'jobs'}},
+  {
+    path: 'news',
+    children: feedRoutes,
+    data: {feedType: 'news'}
+  },
+  {
+    path: 'newest',
+    children: feedRoutes,
+    data: {feedType: 'newest'}
+  },
+  {
+    path: 'show',
+    children: feedRoutes,
+    data: {feedType: 'show'}
+  },
+  {
+    path: 'ask',
+    children: feedRoutes,
+    data: {feedType: 'ask'}
+  },
+  {
+    path: 'jobs',
+    children: feedRoutes,
+    data: {feedType: 'jobs'}
+  },
   {path: 'item', loadChildren: 'app/item-details/item-details.module#ItemDetailsModule'},
   {path: 'user', loadChildren: 'app/user/user.module#UserModule'}
 ];
+
 
 // - Updated Export
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
