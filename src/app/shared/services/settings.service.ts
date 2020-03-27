69c6e74e59cda040430b826d2f9908548df10175
Miroslav Foltýn <miroslav.foltyn64@gmail.com>
2020-03-27 17:35:30 +0100

    added detection of user's system preffered color scheme (#83)

--- src/app/shared/services/settings.service.ts
+++ src/app/shared/services/settings.service.ts
@@ -2,17 +2,66 @@ import { Injectable } from '@angular/core';
 
 import { Settings } from '../models/settings';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class SettingsService {
   settings: Settings = {
     showSettings : false,
     openLinkInNewTab: localStorage.getItem("openLinkInNewTab") ? JSON.parse(localStorage.getItem("openLinkInNewTab")) : false,
-    theme: localStorage.getItem("theme") ? localStorage.getItem("theme") : 'default',
+    theme: 'default',
     titleFontSize: localStorage.getItem("titleFontSize") ? localStorage.getItem("titleFontSize") : '16',
     listSpacing: localStorage.getItem("listSpacing") ? localStorage.getItem("listSpacing") : '0',
   };
 
-  constructor() { }
+  darkColorSchemeMedia = window.matchMedia('(prefers-color-scheme: dark)');
+  
+  constructor() {
+    this.subscribeToSystemPreferredColorScheme();
+    this.initTheme();
+  }
+  
+  ngOnDestroy() {
+    this.unSubscribeToSystemPrefferedColorScheme();
+  }
+
+  handleSystemPreferredColorSchemeChange(event: MediaQueryListEvent) {
+    let theme;
+    if (event.matches) {
+      theme = 'night';
+    } else {
+      theme = 'default';
+    }
+    this.setTheme(theme);
+  }
+  
+  subscribeToSystemPreferredColorScheme() {
+    this.darkColorSchemeMedia.addEventListener(
+      'change',
+      this.handleSystemPreferredColorSchemeChange.bind(this)
+    );
+  }
+
+  initTheme() {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      this.settings.theme = savedTheme;
+    } else {
+      this.darkColorSchemeMedia.dispatchEvent(
+        new MediaQueryListEvent('change', {
+          media: this.darkColorSchemeMedia.media,
+          matches: this.darkColorSchemeMedia.matches
+        })
+      );
+    }
+  }
+
+  unSubscribeToSystemPrefferedColorScheme() {
+    this.darkColorSchemeMedia.removeEventListener(
+      'change',
+      this.handleSystemPreferredColorSchemeChange.bind(this)
+    );
+  }
 
   toggleSettings() {
     this.settings.showSettings = !this.settings.showSettings;
